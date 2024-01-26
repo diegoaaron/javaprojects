1ff8ef6383cb1ecb974f9d66353231c4ad835c39
diego aaron <diegoaarondv@gmail.com>
2024-01-25 23:07:25 -0500

    primeros ejercicios

---
+++ ejercicios1/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] personas = {"Lucas Moy", "Aldo Angelini", "Julia Gomez", "Manuel Santillan",
+                "Juan Torres"};
+
+        System.out.println("Ingrese su búsqueda: ");
+        Scanner teclado = new Scanner(System.in);
+
+        String personaAEncontrar = teclado.nextLine();
+
+        // forma 1
+        /*
+        for(int i = 0; i<personas.length; ++i){
+            if(personaAEncontrar.equalsIgnoreCase(personas[i])){
+                System.out.println("Persona encontrada: " + personas[i] + " en la posicion " + i);
+            }
+        }
+        */
+
+        // forma 2
+        for(String nombrePersona: personas) {
+            if(nombrePersona.contains(personaAEncontrar)) {
+                System.out.println("La persona es: " + nombrePersona);
+            }
+        }
+
+    }
+}
