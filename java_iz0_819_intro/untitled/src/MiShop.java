84d995cf0127fdf494e9803f975abc07e2f670a4
diego aaron <diegoaarondv@gmail.com>
2024-02-21 22:29:55 -0500

    update java intro

---
+++ java_iz0_819_intro/untitled/src/MiShop.java
@@ -0,0 +1,10 @@
+public class MiShop {
+    public static void main(String[] args){
+
+        double x = 0b10;
+        System.out.println(x);
+
+        // el valor null solo existe para las instancias de clase
+    }
+
+}
--- java_iz0_819_intro/untitled/src/Shopu.java
+++ java_iz0_819_intro/untitled/src/Shopu.java
@@ -1,15 +1,14 @@
 public class Shopu {
 
     String name;
-    public  Shopu(String a){
 
-
-
+    {
+        name = "ABC";
     }
 
     public static void main(String[] args) {
-        Shopu s = new Shopu("SD");
-        s.name = "Big shopu LLC";
+        Shopu s = new Shopu();
+        //s.name = "Big shopu LLC";
         System.out.println(s.name);
     }
 }
